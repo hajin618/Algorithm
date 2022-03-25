2b2ae1fac3fac3ef5ee6b368deb27ef28183b6ea
hajin618 <1998rlagkwls@gmail.com>
2022-03-25 22:53:45 +0900

    Baekjoon Online Judge 1309

---
+++ BOJ_1309.cpp
@@ -0,0 +1,25 @@
+//1309
+#include <iostream>
+using namespace std;
+
+#define MOD 9901
+
+int dp[100001][3];
+
+int main() {
+	int n;
+	cin >> n;
+
+	dp[1][0] = 1;
+	dp[1][1] = 1;
+	dp[1][2] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % MOD;
+		dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+	}
+
+	cout << (dp[n][0] + dp[n][1] + dp[n][2]) % MOD;
+	return 0;
+}
